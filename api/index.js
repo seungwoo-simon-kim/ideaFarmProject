135e198fd8a34887ecdfde23ebe472928133bd60
seungwoo-simon-kim <seungwoo.simon.kim@gmail.com>
2020-07-10 08:13:01 +0900

    get dates of monday and friday

--- api/index.js
+++ api/index.js
@@ -93,10 +93,13 @@ api.post("/user/getUserInfo", async (req, res) => {
 api.post("/commute/getUserDateList", async (req, res) => {
     console.log(req.body.getWeek);
     /* getting week */
+    let date = new Date(req.body.date);
     let [ year, month, date ]  = req.body.date.split('-');
     let yoIl = new Date(req.body.date).getDay() - 1;
-    let monday = `${year}-${month}-${date - yoIl}`
-    let friday = `${year}-${month}-${date + (4 - yoIl)}`
+    let monday_date = (date - yoIl <= 9 ? "0" : "") + (date - yoIl);
+    let friday_date = (date - (- 4) - yoIL <= 9 ? "0" : "") + (date - (- 4) - yoIl);
+    let monday = `${year}-${month}-${monday_date}`
+    let friday = `${year}-${month}-${friday_date}`
     console.log(year, month, date, yoIl, monday, friday);
     console.log(`this is monday: ${monday}`);
     console.log(`this is friday: ${friday}`);

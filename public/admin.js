d797fc47205de317e93e2530a16c7a60fb339e67
seungwoo-simon-kim <seungwoo.simon.kim@gmail.com>
2020-07-23 08:48:28 +0900

    just one chart

--- public/admin.js
+++ public/admin.js
@@ -3,17 +3,31 @@ class App {
     constructor() {
         this._loadTable = this._loadTable.bind(this);
         this._onLogin = this._onLogin.bind(this);
+        // this._switchHeaders = this._switchHeaders.bind(this);
     }
 
     setup() {
         this._loginForm = document.querySelector("#loginForm");
         this._welcome = document.querySelector("#welcome");
         this._table = document.querySelector("#myTable");
+        this._tableContents = this._table.children[0];
         this._loginForm.login.addEventListener("click", this._onLogin);
+        // this._loginForm.test.addEventListener("click", this._switchHeaders);
     }
 
+    // _switchHeaders(event) {
+    //     event.preventDefault();
+    //     let headers = this._tableContents.children;
+    //     for (let i = 0; i < headers.length; i++) {
+    //         if (headers[i].classList.contains("hidden")) {
+    //             headers[i].classList.remove("hidden");
+    //         }  else {
+    //             headers[i].classList.add("hidden");
+    //         }
+    //     }
+    // }
+
     _loadTable() {
-        console.log('works');
         this._welcome.classList.add("hidden");
         this._table.classList.remove("hidden");
     }
@@ -25,10 +39,8 @@ class App {
             companyID: form.companyID.value,
             password: form.password.value
         }
-        console.log(body);
-        let res = await apiRequest("POST", `/login`, body);
+        let res = await apiRequest("POST", "/login", body);
         let json = await res.json();
-        console.log(json);
         if (json.result === "true") {
             this._loadTable();
         }  else {
